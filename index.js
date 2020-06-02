080f9f8d4d874f5f5e81b426ca587b4516d88480
dan <dan@localhost.localdomain>
2020-06-02 15:21:49 +0300

    changing a XLSX file into json objects

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const Joi = require('joi')
 const XLSX = require('xlsx')
 const express = require('express')
 const upload = require('express-fileupload')
+const path = require('path')
+const fs = require('fs')
 const app = express();
 app.use(express.json());
 app.use(upload())
-const path = require('path')
 
 
 
@@ -30,19 +31,33 @@ app.post('/api/testy/', (req, res) => {
                 res.send(err)
             } else {
                 // creating the path to our uploaded xlsx file
-                const xlsxPath = { dir: 'uploads',base:`${filename}`}
+                const xlsxPath = { dir: 'uploads', base: `${filename}` }
                 const pathTo = path.format(xlsxPath)
-                
+
                 // reading our xlsx file we have uploaded 
                 const workbook = XLSX.readFile(pathTo)
                 const sheet_name_list = workbook.SheetNames;
                 // converting our data to json format using the sheet_to_json 
                 const results = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
+
+                // store our json data into a .json file 
+                JsonFileFirst = filename.split('.')
+                nameArr = [JsonFileFirst[0], 'json']
+                JsonFilename = nameArr.join('.')
+                JsonFilePathname = { dir: 'uploads', base: `${JsonFilename}` }
+                JsonFilePath = path.format(JsonFilePathname)
+
+
+                fs.appendFile(JsonFilePath, results, function (err) {
+                    if (err) throw err;
+                    console.log('saved')
+                })
+
                 res.status(200).send(results)
             }
         })
 
-    }else{
+    } else {
         res.redirect('/')
     }
 });
