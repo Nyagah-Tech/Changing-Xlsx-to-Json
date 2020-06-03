fef5d0cf1a901106db80135c130cbeb001466791
dan <dan@localhost.localdomain>
2020-06-03 21:53:42 +0300

    stringify json data

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const upload = require('express-fileupload')
 const path = require('path')
 const fs = require('fs')
 const app = express();
+const { promisify } = require('util')
 app.use(express.json());
 app.use(upload())
-app.use('/upload',express.static('uploads'))
+app.use('/upload', express.static('uploads'))
 
 
 
@@ -16,59 +17,62 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 });
 
-app.post('/uploads/file/', (req, res) => {
-    if (req.files) {
-        // checking the file from the req 
-        console.log(req.files)
-        // fetching the file from the req object 
-        var file = req.files.file
-        // getting the name of the file from the file object 
-        var filename = file.name
-        console.log(filename)
+app.post('/uploads/file/', async (req, res) => {
+ 
 
-        // moving the .xlsx file to the uploads directory  
-        file.mv('./uploads/' + filename, function (err) {
-            if (err) {
-                res.send(err)
-            } else {
-                // creating the path to our uploaded xlsx file
-                const xlsxPath = { dir: 'uploads', base: `${filename}` }
-                const pathTo = path.format(xlsxPath)
+if (req.files) {
+    // checking the file from the req 
+    console.log(req.files)
+    // fetching the file from the req object 
+    var file = req.files.file
+    // getting the name of the file from the file object 
+    var filename = file.name
+    console.log(filename)
 
-                // reading our xlsx file we have uploaded 
-                const workbook = XLSX.readFile(pathTo)
-                const sheet_name_list = workbook.SheetNames;
-                // converting our data to json format using the sheet_to_json 
-                const results = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
+    // moving the .xlsx file to the uploads directory  
+    await file.mv('./uploads/' + filename, function (err) {
+        if (err) {
+            res.send(err)
+        } else {
+            // creating the path to our uploaded xlsx file
+            const xlsxPath = { dir: 'uploads', base: `${filename}` }
+            const pathTo = path.format(xlsxPath)
 
-                // spliting the filename into a array of the filename and its file type
-                JsonFileFirst = filename.split('.')
-                // placing our file name with the json extension on same array 
-                nameArr = [JsonFileFirst[0], 'json']
-                // joining the items in the nameArr to form our file.json 
-                JsonFilename = nameArr.join('.')
-                // creating a path to the json file 
-                JsonFilePathname = { dir: 'uploads', base: `${JsonFilename}` }
-                JsonFilePath = path.format(JsonFilePathname)
+            // reading our xlsx file we have uploaded 
+            const workbook = XLSX.readFile(pathTo)
+            const sheet_name_list = workbook.SheetNames;
+            // converting our data to json format using the sheet_to_json 
+            const results = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
+            // stringify json data to be written on the jsonfile 
+            data = JSON.stringify(results)
 
-                // use the appendfile method to write data into our json file as well as create the file 
-                fs.appendFile(JsonFilePath, results, function (err) {
-                    if (err) throw err;
-                    console.log('saved')
-                })
-                fs.writeFile(JsonFilePath,results, (err)=>{
-                    if (err) throw err;
-                })
-                const urlPath = `/upload/${JsonFilename}`
-                res.redirect(urlPath)
-            }
-        })
+            // spliting the filename into a array of the filename and its file type
+            JsonFileFirst = filename.split('.')
+            // placing our file name with the json extension on same array 
+            nameArr = [JsonFileFirst[0], 'json']
+            // joining the items in the nameArr to form our file.json 
+            JsonFilename = nameArr.join('.')
+            // creating a path to the json file 
+            JsonFilePathname = { dir: 'uploads', base: `${JsonFilename}` }
+            JsonFilePath = path.format(JsonFilePathname)
 
-    } else {
-        res.redirect('/')
-    }
+            // use the appendfile method to write data into our json file as well as create the file 
+            fs.appendFile(JsonFilePath,data,function(err){
+                if (err){
+                    console.log(err)
+                }
+            })
+            const urlPath = `/upload/${JsonFilename}`
+            res.redirect(urlPath)
+        }
+    })
+
+} else {
+    res.redirect('/')
+}
+
+
 });
-
 
 //port
 const port = process.env.PORT || 3000
